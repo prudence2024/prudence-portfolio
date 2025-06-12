6a15d40a80ec4faa3c3a4111d180c57e6eb72174
prudence2024 <okwuborprudence@gmail.com>
2025-06-12 20:59:04 +0100

    new

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-// JAVASCRIPT
+// COMMENT
+// GET ALL NAV LINKS
+const navLinks = document.querySelectorAll('.nav__link');
+const menuButton = document.getElementById('menu-button');
+
+// LOOK OVER EACH LINKS TO REMOVE NAVIGATION BAR ON CLICK 
+navLinks.forEach(
+    link=>{
+        link.addEventListener(
+            'click',()=>{
+                if(menuButton && menuButton.checked){
+                    menuButton.checked = false;
+                }
+            }
+        )
+    }
+)
